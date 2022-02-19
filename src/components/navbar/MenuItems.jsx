51b774593ed6a74e7bc6c09c7b85c85d704fe2c3
DME-WebDev <97695960+DME-WebDev@users.noreply.github.com>
2022-02-19 13:28:17 +0100

    Event handlers for dropdown
    On mouse hover, display dropdown. No styling yet. Routes added.

---
+++ src/components/navbar/MenuItems.jsx
@@ -0,0 +1,17 @@
+export const MenuItems = [
+    {
+        title: 'Developer Bootcamp',
+        path: '/devbootcamp',
+        cName: 'dropdown-link'
+    },
+    {
+        title: 'Security Bootcamp',
+        path: '/secbootcamp',
+        cName: 'dropdown-link'
+    },
+    {
+        title: 'Content Management Bootcamp',
+        path: '/mgtbootcamp',
+        cName: 'dropdown-link'
+    }
+]
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,14 +1,33 @@
 import React, { useState } from 'react'
 import './navbar.css'
+import Dropdown from './Dropdown'
 import { Link } from 'react-router-dom'
 import { FaTimes, FaBars, FaPiedPiper } from 'react-icons/fa'
 import { BsFillCaretDownFill } from 'react-icons/bs'
 
 const NavBar = () => {
   const [click, setClick] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
 
   const clickHandler = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+
+  const onMouseEnter = () =>{
+    if(window.innerWidth < 960){
+      setDropdown(false);
+    }
+    else{
+      setDropdown(true);
+    }
+  };
+  const onMouseLeave = () =>{
+    if(window.innerWidth < 960){
+      setDropdown(false);
+    }
+    else{
+      setDropdown(false);
+    }
+  };
 
   return (
   <>
@@ -26,10 +45,10 @@ const NavBar = () => {
           <li className="mobile-menu-items">
             <Link to='/about' className='mobile-links' onClick={closeMobileMenu}>About</Link>
           </li>
-          <li className="mobile-menu-items">
+          <li className="mobile-menu-items" onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
             <Link to='/bootcamps' className='mobile-links' onClick={closeMobileMenu}>Bootcamps
             <BsFillCaretDownFill />
-            
+             {dropdown && <Dropdown />}
             </Link>
           </li>
           <li className="mobile-menu-items">
